733c78caf991570d4d180a8cd289a7ae80555dc9
oldorgan <andy.gorham@outlook.com>
2022-02-14 14:56:40 +0100

    Added a prompt

--- hungry.py
+++ hungry.py
@@ -5,4 +5,8 @@ Created on Mon Feb 14 13:27:21 2022
 @author: Andy
 """
 
-print("I'm hungry")
+hungry=input("are you hungry?")
+if hungry == "yes":
+    print("Eat a Samosa")
+else:
+    print("Do your homework")
